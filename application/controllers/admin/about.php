53eb1a94cb102367e8054859efa73bb1d04b619d
puji rahmadiyano <puji.rahmadiyanto@gmail.com>
2011-06-01 10:13:07 +0700

    about in progress

--- application/controllers/admin/about.php
+++ application/controllers/admin/about.php
@@ -1,13 +1,21 @@
 <?php
-class About extends CI_Controller{
 
-    function __construct(){
+class About extends CI_Controller {
+
+    function __construct() {
         parent::__construct();
-
     }
-    function index(){
+
+    function index() {
+        $this->load->model('about_model');
+        $data['about'] = $this->about_model->get_all_model();
+        $data['content'] = "admin/form_about";
+    }
+
+    function update() {
         
     }
+
 }
 
 ?>
