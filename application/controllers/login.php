0decbfc1d397545fe6d197a3e8954de644ab80d5
puji rahmadiyanto <puji.rahmadiyanto@gmail.com>
2011-06-04 17:35:16 -0400

    uda di tambahi auth

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -2,13 +2,13 @@
 
 class Login extends CI_Controller {
 
-    function  __construct() {
+    function __construct() {
         parent::__construct();
     }
 
-    function index(){
+    function index() {
 
-       $config=  $config = array(
+        $config = $config = array(
             array(
                 'field' => 'username',
                 'label' => 'username',
@@ -19,13 +19,23 @@ class Login extends CI_Controller {
                 'label' => 'password',
                 'rules' => 'required'
             )
-           );
-       $this->form_validation->set_rules($config);
-       if($this->form_validation->run()==TRUE){
-           
-       }
-       $this->load->view('login/logintemplate');
+        );
+        $this->form_validation->set_rules($config);
+        if ($this->form_validation->run() == TRUE) {
+            $this->load->library('auth');
+            $username = $this->input->post('username',TRUE);
+            $password = $this->input->post('password'.TRUE);
+            if($this->auth->proses_login($username,$password)==TRUE){
+                redirect('admin','refresh');
+            }
+            else {
+                $this->session->set_flashdata('notif','username dan password tidak valid');
+                redirect('login/index');
+            }
+        }
+        $this->load->view('login/logintemplate');
     }
+
 }
 
 ?>
